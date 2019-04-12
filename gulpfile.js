a36266d3f38c9f98f9db46a87667743c80c66a4f
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-04-12 08:14:44 -0300

    Alteração na Padronização de Inserção dos dados

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+/*var webserver = require('gulp-webserver');*/
+
+var st = require('st');
+var connect = require('gulp-connect');
+
  
-gulp.task('webserver', function() {
-  gulp.src("./app")
+/*gulp.task('webserver', function() {
+  gulp.src("./app/")
     .pipe(webserver({
       host:"localhost",
       port:8020,
@@ -10,5 +14,23 @@ gulp.task('webserver', function() {
       directoryListing: false,
       open: true,
       /*fallback:"hello.js"*/
-    }));
+    /*}));
+});*/
+
+
+/*Adaptação provisória */
+gulp.task('loadServer', function(){
+	connect.server({
+    root: 'app/',
+    host: 'localhost',
+    port: 8020,
+    livereload: {
+      port: 35929
+    },
+    middleware: function (connect, opt) {
+      return [
+        st({ path: 'node_modules', url: '/node_modules' })
+      ];
+    }
+  })
 });
